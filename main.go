2d6df4b3149264b3947f2f62add59b8062357d6e
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-21 13:35:25 +0430

    create deck type and print method

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+func main() {
+	cards := deck{newCard(), newCard(), "Ace of Diamonds"}
+
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
